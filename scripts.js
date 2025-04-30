d86d8758682be0b1313f17650fb14ef44bf570f1
Crasheldark <ertiojulito@gmail.com>
2025-04-30 10:46:12 +0200

    Variable hola mundo

--- scripts.js
+++ scripts.js
@@ -2,4 +2,7 @@ window.addEventListener('load', function() {
     // 1. Una variable numero cuyo valor sea -4.
     let numero = -4;
 
+    // 2. Una variable palabra cuyo valor sea ”Hola Mundo”
+    let palabra = "Hola Mundo";
+
 });
